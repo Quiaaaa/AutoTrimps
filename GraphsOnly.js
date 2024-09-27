661a76715f56a16ce8b01a1fe37d1e96d115cded
Quiaaaa <96918270+Quiaaaa@users.noreply.github.com>
2024-09-26 22:20:37 -0600

    Complete restructure

--- GraphsOnly.js
+++ GraphsOnly.js
@@ -1,28 +1,12 @@
-var basepath = 'https://Quiaaaa.github.io/AutoTrimps/' //Link to your own Github here if you forked!
+var graphsBasePath = 'https://Quiaaaa.github.io/AutoTrimps/' //Link to your own Github here if you forked!
+//var graphsBasePath = '/AutoTrimps/'
 
-//var isSteam = false;
-
-function ATscriptLoad(path, module) {
-  if (module == null) graphsDebug('Wrong Syntax. Script could not be loaded.')
-  if (path == null) path = '';
-  var scriptElem = document.createElement('script');
-  scriptElem.src = basepath + path + module + '.js'
-  scriptElem.id = module + '_MODULE'
-  document.head.appendChild(scriptElem)
+function injectScript(id, src) {
+	const script = document.createElement('script');
+	script.id = id;
+	script.src = src;
+	script.setAttribute('crossorigin', 'anonymous');
+	document.head.appendChild(script);
 }
 
-function initializeGraphs() {
-  ATscriptLoad('', 'Graphs');
-  graphsDebug('AutoTrimps - Zek Graphs Only Fork Loaded!', '*spinner3');
-}
-
-var enableGraphsDebug = false;
-function graphsDebug(message) {
-  if (enableGraphsDebug)
-    console.debug(...arguments);
-}
-
-if (typeof MODULES === 'undefined') MODULES = {}
-var startupDelay = 1000;
-setTimeout(initializeGraphs, startupDelay);
-
+setTimeout(() => injectScript('Graphs', graphsBasePath + 'Graphs.js'), 1000);
